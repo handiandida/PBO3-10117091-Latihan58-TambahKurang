0f90f57bde346241372f8fe6956a99b23837754a
Dida Handian <handiandida@gmail.com>
2018-10-29 20:59:13 +0700

    TambahKurang

---
+++ src/pbo3/pkg10117091/latihan58/tambahkurang/PBO310117091Latihan58TambahKurang.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * NAMA  : Dida Handian
+ * Kelas : PBO3
+ * NIM   : 10117091
+ * Deskripsi Tugas : Membuat Program Tambah Kurang
+ *  
+ */
+public class PBO310117091Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumbil = new JumlahBilangan();
+        jumbil.tampilHasilJumlah();
+        
+        SelisihBilangan selisih = new SelisihBilangan();
+        selisih.tampilSelisih();
+    }
+    
+}
